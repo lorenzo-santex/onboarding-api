7a0a85476f8a619a2624ada8ba34a87c30516087
lorenzo-santex <lorenzo.sauchelli@santexgroup.com>
2021-12-02 16:20:39 -0300

    Cuenta

--- models/cuenta.js
+++ models/cuenta.js
@@ -11,12 +11,23 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Cuenta.hasMany(models.CanalSecundario, {
+        foreignKey: {
+          name: 'cuenta_id',
+          comment: 'Foreign Key: company.subledger_empresa.',
+          allowNull: false,
+          references: {
+            model: Cuenta,
+            key: 'cuenta_id'
+          }
+        }
+      });
     }
   };
   Cuenta.init({
     nombre: DataTypes.STRING,
     pm: DataTypes.STRING,
-    canal: DataTypes.STRING,
+    canal_principal: DataTypes.STRING,
     bienvenida: DataTypes.STRING
   }, {
     sequelize,
